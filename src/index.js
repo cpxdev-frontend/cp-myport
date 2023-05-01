2ce68deb147fe3166094f4e1ef51b15f6516d23b
Chinnathorn P <chinnathorn.p@gmail.com>
2023-05-01 20:55:05 +0700

    Major Update 2023

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import {
+  BrowserRouter
+} from "react-router-dom";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'anakotmai',
+  },
+  palette: {
+    secondary: {
+      light: '#fff',
+      main: '#1976d2',
+      contrastText: '#fff',
+    },
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <App/>
+    </ThemeProvider>
+</BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
